f090f99f009f3589b81d5c322c0e10a406342b7c
bogdy9912 <bogdancons99@gmail.com>
2019-04-26 15:43:44 +0300

    racheta face pe figuranta cand se duce dupa submarin

---
+++ Submarine_Missles/Assets/Scripts/TouchPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TouchPlayerController : MonoBehaviour
+{
+    public float copy =0;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        copy = MobileJoystick_UI.instance.moveDirection.y;
+        copy = Mathf.Abs(copy);
+        //Move Front/Back
+        /* if (MobileJoystick_UI.instance.moveDirection.y != 0)
+         {
+             transform.Translate(transform.forward * Time.deltaTime * 2.45f * MobileJoystick_UI.instance.moveDirection.y, Space.World);
+         }
+         */
+    
+        
+            transform.Translate(transform.forward * Time.deltaTime * 2.45f * 1, Space.World);
+        
+        //Rotate Left/Right
+        if (MobileJoystick_UI.instance.moveDirection.x != 0)
+        {
+            transform.Translate(transform.right * Time.deltaTime * 2.45f * MobileJoystick_UI.instance.moveDirection.x, Space.World);
+            if (MobileJoystick_UI.instance.moveDirection.x>0)
+            transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * 4.5f * 1, Space.Self);
+            else
+                transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * 4.5f * (-1), Space.Self);
+        }
+    }
+}
---
+++ Submarine_Missles/Assets/Scripts/script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public static int playerHealth = 1;
+    public static int enemyHealth = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
